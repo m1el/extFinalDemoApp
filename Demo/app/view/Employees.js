85b8afc7b02658772c02ab34b2a9c569839ede73
my8bit <mail@my8bit.name>
2013-10-08 10:31:56 +0300

    grid issu

--- Demo/app/view/Employees.js
+++ Demo/app/view/Employees.js
@@ -1,41 +1,29 @@
-Ext.create("Ext.data.Store", {
-	storeId: "simpsonsStore",
-	fields: ["name", "email", "phone"],
-	data: {
-		"items": [{
-			"name": "Lisa",
-			"email": "lisa@simpsons.com",
-			"phone": "555-111-1224"
-		}, {
-			"name": "Bart",
-			"email": "bart@simpsons.com",
-			"phone": "555-222-1234"
-		}, {
-			"name": "Homer",
-			"email": "home@simpsons.com",
-			"phone": "555-222-1244"
-		}, {
-			"name": "Marge",
-			"email": "marge@simpsons.com",
-			"phone": "555-222-1254"
-		}]
-	},
-	proxy: {
-		type: "memory",
-		reader: {
-			type: "json",
-			root: "items"
-		}
-	}
-});
-
 Ext.define("Demo.view.Employees", {
-	extend: "Ext.Component",
+	extend: "Ext.container.Container",
 	//html: "Hello, Emp!!",
 	items: [{
 		xtype: "grid",
 		title: "Simpsons",
-		store: Ext.data.StoreManager.lookup("simpsonsStore"),
+		store: Ext.create("Ext.data.JsonStore", {
+			fields: ["name", "email", "phone"],
+			data: [{
+				"name": "Lisa",
+				"email": "lisa@simpsons.com",
+				"phone": "555-111-1224"
+			}, {
+				"name": "Bart",
+				"email": "bart@simpsons.com",
+				"phone": "555-222-1234"
+			}, {
+				"name": "Homer",
+				"email": "home@simpsons.com",
+				"phone": "555-222-1244"
+			}, {
+				"name": "Marge",
+				"email": "marge@simpsons.com",
+				"phone": "555-222-1254"
+			}]
+		}),
 		columns: [{
 			text: "Name",
 			dataIndex: "name"
